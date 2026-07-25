e7ee9f083bcd7a3f4c05d9dce759ef8d579135ad
Magairisarri <magairisarri@gmail.com>
2026-07-25 12:42:56 -0300

    PoC Cypress: setup inicial y tests sobre saucedemo

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    setupNodeEvents(on, config) {},
+  },
+});
