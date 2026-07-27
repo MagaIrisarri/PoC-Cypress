6881dada2c4980c7de7add1c9516c78a1412b721
Magairisarri <magairisarri@gmail.com>
2026-07-27 20:09:00 -0300

    Configuracion del reporter y gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  reporter: 'cypress-mochawesome-reporter',
 
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
